fe1f384adf1854c0f58dc6fea4e548727e3cb558
James <jamesl.escudero@gmail.com>
2024-11-26 12:54:17 -0800

    Simple Calc Ver 1
    - Direct var Calc

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] theArgs) {
+        // First Run
+        System.out.println("Test Water");
+        int num1 = 5;
+        int num2 = 2;
+        System.out.println(num1 + num2);
+    }
+}
