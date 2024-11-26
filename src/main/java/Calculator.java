ede9057abf10acbcc672f1798ec143371f7ccd0e
James <jamesl.escudero@gmail.com>
2024-11-26 13:57:28 -0800

    Refactor sections into Methods
    - Method to handle Clint num
    - Method to handle Operation Choices
    - Method to hanlde Calc Continue

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,45 +1,61 @@
 import java.util.Scanner;
 
 public class Calculator {
+
     public static void main(String[] theArgs) {
         boolean repeat = true;
+        Scanner input = new Scanner(System.in);
 
         do {
             // Asking clint their 2 numbers
             System.out.println("What is the first number?");
-            Scanner input = new Scanner(System.in);
-            int num1 = input.nextInt();
+            int num1 = ClintNumInput(input);
             System.out.println("What is the second number?");
-            int num2 = input.nextInt();
+            int num2 = ClintNumInput(input);
 
             // Asking clint what operation
             System.out.print("What operation? \n");
             System.out.println("Addition, Subtraction, Multiplication, Division");
             System.out.println("Choose from 1, 4");
-            int operation = input.nextInt();
+            int operation = ClintNumInput(input);
 
             // Printing
             System.out.print("This is the total of the two numbers: \n");
-
-            // Logic for operation
-            if (operation == 1) {
-                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-            } else if (operation == 2) {
-                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-            } else if (operation == 3) {
-                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-            } else {
-                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-            }
+            OperationOutput(operation, num1, num2);
 
             // Asking user to continue
-            System.out.println("Do you wish to continue? (y/n)");
-            String answer = input.next();
-            if (answer.equals("n")) {
-                repeat = false;
-            }
+            repeat = continuationStatus(input, repeat);
+
         } while (repeat);
     }
+
+    public static int ClintNumInput (Scanner theInput) {
+        return theInput.nextInt();
+    }
+
+    public static void OperationOutput(int theOperation, int theNum1, int theNum2) {
+        // Logic for operation
+        if (theOperation == 1) {
+            System.out.println(theNum1 + " + " + theNum2 + " = " + (theNum1 + theNum2));
+        } else if (theOperation == 2) {
+            System.out.println(theNum1 + " - " + theNum2 + " = " + (theNum1 - theNum2));
+        } else if (theOperation == 3) {
+            System.out.println(theNum1 + " * " + theNum2 + " = " + (theNum1 * theNum2));
+        } else {
+            System.out.println(theNum1 + " / " + theNum2 + " = " + (theNum1 / theNum2));
+        }
+    }
+
+    public static boolean continuationStatus(Scanner input, boolean theRepeat) {
+        boolean status = true;
+        System.out.println("Do you wish to continue? (y/n)");
+        String answer = input.next();
+        if (answer.equals("n")) {
+            status = false;
+        }
+        return status;
+    }
+
 
     public static void SimpleCalcV1() {
         // First Run
