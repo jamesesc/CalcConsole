4779ebfa869a131514a6467b01dc214fa792a697
James <jamesl.escudero@gmail.com>
2024-11-26 13:00:35 -0800

    SimpleCalcV1 move to Method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,16 @@
 public class Calculator {
     public static void main(String[] theArgs) {
+
+    }
+
+    public static void SimpleCalcV1() {
         // First Run
         System.out.println("Test Water");
         int num1 = 5;
         int num2 = 2;
         System.out.println(num1 + num2);
+
     }
+
+
 }
