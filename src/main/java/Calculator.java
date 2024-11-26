f440e5d910d0d59ed72228096937647529888f8e
James <jamesl.escudero@gmail.com>
2024-11-26 13:23:14 -0800

    Implemented user choice of operation
    - Include, Add, Subtract, Multiply, Division operation choices
    - Each operation have their own unique output

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,17 +2,32 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] theArgs) {
+        // Asking clint their 2 numbers
         System.out.println("What is the first number?");
         Scanner input = new Scanner(System.in);
         int num1 = input.nextInt();
         System.out.println("What is the second number?");
         int num2 = input.nextInt();
+
+        // Asking clint what operation
+        System.out.print("What operation? \n");
+        System.out.println("Addition, Subtraction, Multiplication, Division");
+        System.out.println("Choose from 1, 4");
+        int operation = input.nextInt();
+
+        // Printing
         System.out.print("This is the total of the two numbers: \n");
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
 
-
-
-
+        // Logic for operation
+        if (operation == 1) {
+            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        } else if (operation == 2) {
+            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+        } else if (operation == 3) {
+            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+        } else {
+            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+        }
 
     }
 
@@ -22,8 +37,6 @@ public class Calculator {
         int num1 = 5;
         int num2 = 2;
         System.out.println(num1 + num2);
-
     }
-
 
 }
