f108e530ddf8ecd3f27a18342a6dd475aee57ed8
James <jamesl.escudero@gmail.com>
2024-11-26 13:33:01 -0800

    Implemented Continuation operation
    - Added a do/while loop to forever continue
    - Until clint says 'n' to stop

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,33 +2,43 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] theArgs) {
-        // Asking clint their 2 numbers
-        System.out.println("What is the first number?");
-        Scanner input = new Scanner(System.in);
-        int num1 = input.nextInt();
-        System.out.println("What is the second number?");
-        int num2 = input.nextInt();
+        boolean repeat = true;
 
-        // Asking clint what operation
-        System.out.print("What operation? \n");
-        System.out.println("Addition, Subtraction, Multiplication, Division");
-        System.out.println("Choose from 1, 4");
-        int operation = input.nextInt();
+        do {
+            // Asking clint their 2 numbers
+            System.out.println("What is the first number?");
+            Scanner input = new Scanner(System.in);
+            int num1 = input.nextInt();
+            System.out.println("What is the second number?");
+            int num2 = input.nextInt();
 
-        // Printing
-        System.out.print("This is the total of the two numbers: \n");
+            // Asking clint what operation
+            System.out.print("What operation? \n");
+            System.out.println("Addition, Subtraction, Multiplication, Division");
+            System.out.println("Choose from 1, 4");
+            int operation = input.nextInt();
 
-        // Logic for operation
-        if (operation == 1) {
-            System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
-        } else if (operation == 2) {
-            System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
-        } else if (operation == 3) {
-            System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
-        } else {
-            System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
-        }
+            // Printing
+            System.out.print("This is the total of the two numbers: \n");
 
+            // Logic for operation
+            if (operation == 1) {
+                System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+            } else if (operation == 2) {
+                System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+            } else if (operation == 3) {
+                System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
+            } else {
+                System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+            }
+
+            // Asking user to continue
+            System.out.println("Do you wish to continue? (y/n)");
+            String answer = input.next();
+            if (answer.equals("n")) {
+                repeat = false;
+            }
+        } while (repeat);
     }
 
     public static void SimpleCalcV1() {
