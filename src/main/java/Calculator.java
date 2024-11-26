e21d8ebf57057c97ecfa5e18acf033e8a629322b
James <jamesl.escudero@gmail.com>
2024-11-26 13:16:24 -0800

    Calc V2 - User Input
    - Adding user interaction
    - Asking the numbers they want to add

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] theArgs) {
+        System.out.println("What is the first number?");
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        System.out.println("What is the second number?");
+        int num2 = input.nextInt();
+        System.out.print("This is the total of the two numbers: \n");
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+
+
+
+
 
     }
 
