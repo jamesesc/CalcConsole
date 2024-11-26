74d4a070ef2187f98ee1c42b9f679386dc099ecb
James <jamesl.escudero@gmail.com>
2024-11-26 14:06:04 -0800

    Add Comments and Info
    - Annoted comments for the logic

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,40 +1,44 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importation to read console
 
 public class Calculator {
 
     public static void main(String[] theArgs) {
-        boolean repeat = true;
+        // Declaring Scanner as input; Instantiation of the Scanner by keyword 'new'
         Scanner input = new Scanner(System.in);
+        // Boolean for calc repeat status
+        boolean repeat = true;
 
+        // Do-While Loop for Calc continuation functionality
         do {
-            // Asking clint their 2 numbers
+            // Asking the client their 2 numbers
             System.out.println("What is the first number?");
             int num1 = ClintNumInput(input);
             System.out.println("What is the second number?");
             int num2 = ClintNumInput(input);
 
-            // Asking clint what operation
+            // Asking the client what operation
             System.out.print("What operation? \n");
-            System.out.println("Addition, Subtraction, Multiplication, Division");
+            System.out.println("1) Addition \n 2) Subtraction \n 3) Multiplication \n 4) Division");
             System.out.println("Choose from 1, 4");
             int operation = ClintNumInput(input);
 
-            // Printing
-            System.out.print("This is the total of the two numbers: \n");
+            // Outputting the results of the operation
+            System.out.print("This is answer \n");
             OperationOutput(operation, num1, num2);
 
-            // Asking user to continue
+            // Asking the client to continue (y/n)
             repeat = continuationStatus(input, repeat);
-
         } while (repeat);
     }
 
+    // Method to read the console for int, and returning that int
     public static int ClintNumInput (Scanner theInput) {
         return theInput.nextInt();
     }
 
+    // Method to handle the operation
     public static void OperationOutput(int theOperation, int theNum1, int theNum2) {
-        // Logic for operation
+        // theOperation # is base on the choices of operation
         if (theOperation == 1) {
             System.out.println(theNum1 + " + " + theNum2 + " = " + (theNum1 + theNum2));
         } else if (theOperation == 2) {
@@ -46,6 +50,7 @@ public class Calculator {
         }
     }
 
+    // Method to handle Calc continuation functionality
     public static boolean continuationStatus(Scanner input, boolean theRepeat) {
         boolean status = true;
         System.out.println("Do you wish to continue? (y/n)");
@@ -56,7 +61,7 @@ public class Calculator {
         return status;
     }
 
-
+    // Very first Calc Design
     public static void SimpleCalcV1() {
         // First Run
         System.out.println("Test Water");
@@ -64,5 +69,4 @@ public class Calculator {
         int num2 = 2;
         System.out.println(num1 + num2);
     }
-
 }
